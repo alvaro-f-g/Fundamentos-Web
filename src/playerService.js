ab1606ba9b4b3a4a53c579b93143253e19e1e63d
RisiGT <sanchez.jorge02@gmail.com>
2024-01-04 00:52:41 +0100

    playersNames añadido
    Es un conjunto que evita inscribir jugadores con el mismo nombre

--- src/playerService.js
+++ src/playerService.js
@@ -1,11 +1,14 @@
 let players = new Map();
+let playersNames = new Set();
 let nextId = 0;
 let freesIdArray = [];
 
 export function addPlayer(player) {
+    isNew(player.name);
     player.checkValues();
 
     player.id = freesIdArray.length ? freesIdArray.pop() : nextId++;  // Si hay ids que han quedado libres se usará uno de ellos, sino se creará uno nuevo
+    playersNames.add(player.name);
     players.set(player.id, player);
 }
 
@@ -16,6 +19,9 @@ export function addSubelement(player, subelement) {
 }
 
 export function editPlayer(oldPlayer, newPlayer) {
+    if (newPlayer.name != oldPlayer.name) isNew(newPlayer.name);
+    playersNames.delete(oldPlayer.name);
+    playersNames.add(newPlayer.name);
     newPlayer.checkValues();
 
     newPlayer.subelements = oldPlayer.subelements;
@@ -34,4 +40,8 @@ export function getPlayers() {
 
 export function getPlayer(id) {
     return players.get(id);
+}
+
+function isNew(name) {
+    if (playersNames.has(name)) throw new Error("Ya existe un jugador con ese nombre");
 }
