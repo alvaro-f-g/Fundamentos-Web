e8a2be53636bc4355d0999dcd32030fc6f2185bf
RisiGT <sanchez.jorge02@gmail.com>
2024-01-17 21:06:13 +0100

    positionFilter
    Además, he mejorado la estética de los botones.

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 const playerWidth = 250 + 2*20 + 2*1; // Ancho máximo de cada jugador + márgenes + bordes
 let isGridColumns = localStorage.getItem('isGridColumns') ? localStorage.getItem('isGridColumns') === "true" : true;
+let selectedPosition;
 let players;
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -7,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     setFormat();
     loadPlayers();
+    filter();
 });
 
 function calculateColumns() {
@@ -35,6 +37,7 @@ async function loadPlayers() {
     if (from == 0) {players.innerHTML = newPlayers}
     else {players.insertAdjacentHTML("beforeend", newPlayers)}
 
+    filterPlayersByPosition(selectedPosition);
     checkLoadMore();
 }
 
@@ -70,4 +73,49 @@ function changeGrid() {
     isGridColumns = !isGridColumns;
     localStorage.setItem('isGridColumns', isGridColumns);
     setFormat();
+}
+
+function filter() {
+    const positionFilter = document.getElementById('positionFilter');
+    positionFilter.addEventListener('click', function (event) {
+        selectedPosition = event.target.dataset.value;
+        filterPlayersByPosition(selectedPosition);
+    });
+}
+
+function checkFilterMessage() {
+    const filterMessage = document.getElementById("filter-message");
+    filterMessage && filterMessage.remove();
+}
+
+
+function filterPlayersByPosition(position) {
+    let hidden = false;
+    let shown = false;
+
+    checkFilterMessage();
+
+    if (position === undefined) {
+        players.querySelectorAll(`a`).forEach(player => {
+            player.style.display = 'block';
+        });
+        return;
+    }
+
+    const selectedPlayers = players.querySelectorAll(`a[data-position="${position}"]`);
+    selectedPlayers.forEach(player => {
+        player.style.display = 'block';
+        shown = true;
+    });
+
+    const otherPlayers = players.querySelectorAll(`a:not([data-position="${position}"])`);
+    otherPlayers.forEach(player => {
+        player.style.display = 'none';
+        hidden = true;
+    });
+
+    if (!shown && hidden) {
+        if (position === "Banquillo") {players.insertAdjacentHTML("beforeend", "<p id='filter-message'>No hay jugadores en el banquillo</p>")}
+        else {players.insertAdjacentHTML("beforeend", "<p id='filter-message'>No hay jugadores que juegen de "+ position.toLowerCase() +"</p>")}
+    }
 }
